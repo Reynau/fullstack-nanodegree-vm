71adec3909a7f86670459ca20ed2303124b10ba0
Xavier Lopez Reynau <xavi.lopez.reynau@gmail.com>
2019-03-20 20:05:37 +0100

    Add Items functionality

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -56,6 +56,59 @@ def deleteCategory(category_id):
         return redirect(url_for('home'))
 
 
+
+
+@app.route('/items/<int:item_id>/')
+def item(item_id):
+    item = session.query(Item).get(item_id)
+    return render_template('item.html', item=item)
+
+@app.route('/items/new/', methods=['GET', 'POST'])
+def newItem():
+    categories = session.query(Category).all()
+    if request.method == 'GET':
+        return render_template('new_item.html', categories=categories)
+    else:
+        category_id = request.form['category']
+        category = session.query(Category).get(category_id)
+        newItem = Item(name = request.form['name'],
+            description = request.form['description'],
+            price = request.form['price'],
+            category = category)
+        session.add(newItem)
+        session.commit()
+        return redirect(url_for('item', item_id = newItem.id))
+
+@app.route('/items/<int:item_id>/edit/', methods=['GET', 'POST'])
+def editItem(item_id):
+    categories = session.query(Category).all()
+    item = session.query(Item).get(item_id)
+    if request.method == 'GET':
+        return render_template('edit_item.html', categories=categories, item=item)
+    else:
+        category_id = request.form['category']
+        category = session.query(Category).get(category_id)
+
+        item.name = request.form['name']
+        item.description = request.form['description']
+        item.price = request.form['price']
+        item.category = category
+        session.add(item)
+        session.commit()
+        return redirect(url_for('item', item_id = item_id))
+
+@app.route('/items/<int:item_id>/delete/', methods=['GET', 'POST'])
+def deleteItem(item_id):
+    item = session.query(Item).get(item_id)
+    category = session.query(Category).get(item.category_id)
+    if request.method == 'GET':
+        return render_template('delete_item.html', item=item)
+    else:
+        session.delete(item)
+        session.commit()
+        return redirect(url_for('category', category_id=category.id))
+
+
 if __name__ == '__main__':
     app.debug = True
     app.run(host = '0.0.0.0', port = 5000)
